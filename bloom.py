677a6136863069465b715fe66256a215bcffd446
hljyunxi <hljyunxi@126.com>
2014-07-04 20:06:42 +0800

    add redis bloomfilter implementation

--- bloom.py
+++ bloom.py
@@ -4,5 +4,46 @@
 
 
 #在redis的基础上构建的bloom filter
+import redis
+
+class Bloom(object):
+    def __init__(self, conn, name, capacity, error_rate=0.001):
+        self.conn = conn
+
+    def __contains__(self, key):
+        pass
+
+    def add(self, key):
+        pass
+
+    @property
+    def count():
+        pass
 
 
+def FNVHash(key):
+    fnv_prime = 0x811C9DC5
+    hash = 0
+    for i in range(len(key)):
+      hash *= fnv_prime
+      hash ^= ord(key[i])
+    return hash
+
+
+def APHash(key):
+    hash = 0xAAAAAAAA
+    for i in range(len(key)):
+      if ((i & 1) == 0):
+        hash ^= ((hash <<  7) ^ ord(key[i]) * (hash >> 3))
+      else:
+        hash ^= (~((hash << 11) + ord(key[i]) ^ (hash >> 5)))
+    return hash
+
+def convert_bytes(bytes):
+    bytes = float(bytes)
+    if bytes > :
+    elif:
+    elif:
+    else:
+
+    return size
--- refactor_hash.py
+++ refactor_hash.py
@@ -2,7 +2,7 @@
 #coding: utf8
 #Author: chenyunyun<hljyunxi@gmail.com>
 
-#redis based refactor hash: http://antirez.com/post/redis-weekly-update-7.html
+#redis hash refactor: http://antirez.com/post/redis-weekly-update-7.html
 
 class RefactorHash(object):
     def __init__(self):
