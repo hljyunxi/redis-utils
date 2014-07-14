0209587a2ccf6d01dd7d0d74611d17f651d830d2
hljyunxi <hljyunxi@126.com>
2014-07-14 09:26:44 +0800

    rename adaptive_counter to adaptive_limiter

--- adaptive_counter.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-#coding: utf8
-#Author: chenyunyun<hljyunxi@gmail.com>
-
-#自适应的redis计数器
---
+++ adaptive_limiter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#coding: utf8
+#Author: chenyunyun<hljyunxi@gmail.com>
+
+#自适应的redis计数器,基于zset
+
+class AdaptiveLimiter(object):
+    def __init__(self):
+        pass
+
+
