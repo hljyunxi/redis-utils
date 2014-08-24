ab21f3d5b13dbd4af1065c54bc604ffecca52343
hljyunxi <hljyunxi@126.com>
2014-08-24 10:57:25 +0800

    add id generator, redis lua wrapper

---
+++ id_generator.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+#coding: utf8
+#Author: chenyunyun<hljyunxi@gmail.com>
+
+
---
+++ redis_lua.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+#coding: utf8
+#Author: chenyunyun<hljyunxi@gmail.com>
+
+#redis lua封装
+
